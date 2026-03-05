b5b401efe2ab45c5a5dc465d952a1366a3cd4ed7
dariann-ss <dariansoto2009@gmail.com>
2026-03-05 15:41:09 +0000

    finished

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ console.log("Script started");
 // Variable to track the number of clicks
 let clicks = 0;
 
+// Value of each click
+let clickValue = 1
+
 // Variable to store the click counter h1
 let clickDisplay = document.getElementById("click-display");
+// Variable to store the level display
+let levelDisplay = document.getElementById("level-display");
+
 
 //Set initial text for click counter
 clickDisplay.innerText = "Clicks: " + clicks;
@@ -14,13 +20,15 @@ function handleClick() {
     console.log("click");
 
     //Add one to click count
-    clicks = clicks + 1;
+    clicks = clicks + clickValue;
 
     //Update display
     clickDisplay.innerText = "Clicks: " + clicks;
 
 // Level 1 complete alert
     if(clicks == 10) {
-    alert("10 Clicks! Level 1 Complete. ")
+    alert("Good Boy! You made it to level 2! ")
+    levelDisplay.innerText = "Level 2";
+    clickValue = 2;
 }
 }
